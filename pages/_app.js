894d9d189d444926743bc755218887225cee1598
stone-wang <49772781+drinkmooon@users.noreply.github.com>
2020-12-10 00:11:00 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Head>
+    <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/3.2.0/antd.min.css' />
+    <Component {...pageProps} />
+  </Head>)
 }
 
 export default MyApp
